2698d7d2f1400fb3002a102d3455fb8d87267735
Dan Farnsworth <absltkaos@gmail.com>
2024-10-23 01:44:22 -0600

    Add support for uploading torrent files instead of URL

--- sonicbit/enums.py
+++ sonicbit/enums.py
@@ -9,7 +9,7 @@ class FileCommand(str, Enum):
 class TorrentCommand(str, Enum):
     ADD_TORRENT_URL = "addTorrentByURL"
     DELETE_TORRENT = "deleteTorrent"
-
+    UPLOAD_TORRENT_FILE = "UploadTorrentFile"
 
 class RemoteDownloadCommand(str, Enum):
     LIST_REMOTE_DOWNLOADS = "get_rdl_task_list"
